d6f35c4124dc25fe3cbc770f67c418d46bd94e7a
oguzcoll <oguzcolak2000@gmail.com>
2024-05-05 16:23:11 +0300

    animation and audio added to the main screen

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -6,8 +6,14 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var scalePlayButton = false
+    @State private var moveBackgroundImage = false
+    
+    
     var body: some View {
         GeometryReader { geo in
             ZStack{
@@ -15,6 +21,12 @@ struct ContentView: View {
                     .resizable()
                     .frame(width: geo.size.width * 3, height: geo.size.height)
                     .padding(.top,3)
+                    .offset(x: moveBackgroundImage ? geo.size.width / 1.2 : -geo.size.width)
+                    .onAppear{
+                        withAnimation(.linear(duration: 60).repeatForever()){
+                            moveBackgroundImage.toggle()
+                        }
+                    }
                
                 VStack{
                    
@@ -67,8 +79,14 @@ struct ContentView: View {
                                 .padding(.vertical,7)
                                 .padding(.horizontal,50)
                                 .background(.brown)
-                                .cornerRadius(7)
+                                .clipShape(RoundedRectangle(cornerRadius: 15))
                                 .shadow(radius: 5)
+                        }
+                        .scaleEffect(scalePlayButton ? 1.2 : 1)
+                        .onAppear{
+                            withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
+                                scalePlayButton.toggle()
+                            }
                         }
                         
                         Spacer()
@@ -89,6 +107,16 @@ struct ContentView: View {
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear{
+            playAudio()
+        }
+    }
+    
+    private func playAudio(){
+        let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
+        audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
     }
 }
 
