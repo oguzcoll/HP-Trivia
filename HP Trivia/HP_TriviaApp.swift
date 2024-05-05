9489ee8e0279c4016fb6bebb640dd3f48d9b686d
oguzcoll <oguzcolak2000@gmail.com>
2024-05-05 15:27:19 +0300

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Oguz Colak on 5.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
